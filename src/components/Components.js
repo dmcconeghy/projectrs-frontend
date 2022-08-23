d41b07aab1021a9d38906a0eba7a8a13efb4b9c4
Dave McConeghy <david.mcconeghy@gmail.com>
2022-08-23 09:50:29 -0400

    Adds maintenance mode

--- src/components/Components.js
+++ src/components/Components.js
@@ -13,6 +13,7 @@ import NavBar from "./NavBar";
 import Breadcrumbs from "./Breadcrumbs";
 import Footer from "./Footer";
 import TagDetails from "./TagDetails";
+import Maintenance from "./Maintenance";
 
 function Components(){
     return (
@@ -22,14 +23,15 @@ function Components(){
             <Breadcrumbs />
 
             <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/podcasts" element={<Podcasts />} />
+                {/* <Route path="/home" element={<Homepage />} /> */}
+                <Route path="/" element={<Maintenance />} />
+                {/* <Route path="/podcasts" element={<Podcasts />} />
                 <Route path="/podcasts/:slug_or_id" element={<EpisodeDetails />} />
                 <Route path="/tags" element={<Tags />} />
                 <Route path="/tags/:slug_or_id" element={<TagDetails />} />
                 <Route path="/contributors" element={<Contributors />} />
                 <Route path="/contributors/:slug_or_id" element={<ContributorDetails />} />
-                <Route path="/responses" element={<Responses />} />
+                <Route path="/responses" element={<Responses />} /> */}
                 <Route path = "*" element={<Navigate to="/" />} />
             </Routes>
 
---
+++ src/components/Maintenance.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Maintenance.css";
+
+function Maintenance() {
+    return (
+        <div className="container">
+        <div className="row">
+            
+            <h1>We're currently working on something.</h1>
+            <p>
+                We'll be back up soon.
+            </p>
+            
+        </div>
+        </div>
+    );
+}
+
+export default Maintenance;
